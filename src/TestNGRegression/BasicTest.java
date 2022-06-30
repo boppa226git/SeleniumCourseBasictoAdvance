9303e710cd6570899afa07ce394482da2291eea6
madhu <boppa226git@gmail.com>
2022-06-30 19:19:53 +0530

    code

--- src/TestNGRegression/BasicTest.java
+++ src/TestNGRegression/BasicTest.java
@@ -1,9 +1,8 @@
 package TestNGRegression;
-import org.testng.annotations.AfterSuite;
+
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
 public class BasicTest {
 	
 @BeforeTest
